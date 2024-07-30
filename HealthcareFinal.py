ace94bdbc86ceaefa5e0b562a6d14414de79d44f
hadilfs <145138634+hadilfs@users.noreply.github.com>
2024-07-30 20:14:29 +0300

    HealthcareFinal.py

--- HealthcareFinal.py
+++ HealthcareFinal.py
@@ -2,4 +2,4 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 st.title("Assessing the Risk Factors on Type 2 Diabetes in the North Africa and Middle East Region")
-
+df = pd.read_csv("https://github.com/hadilfs/Healthcare/blob/main/IHME-GBD_2021_DATA-7e856563-1.csv", delimiter=',')
