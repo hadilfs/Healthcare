ff3b294de32b59fba1a281841741e6ed7f076269
hadilfs <145138634+hadilfs@users.noreply.github.com>
2024-07-30 19:26:15 +0300

    HealthcareFinal.py

--- HealthcareFinal.py
+++ HealthcareFinal.py
@@ -2,3 +2,5 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 df = pd.read_csv("https://github.com/hadilfs/Healthcare/blob/main/IHME-GBD_2021_DATA-7e856563-1.csv")
+st.title("Visualization mortality rate in Lebanon")
+
