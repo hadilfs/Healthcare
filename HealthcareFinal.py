b756fd968c9b53eec0d37a0f4ff4000131eb5ca0
hadilfs <145138634+hadilfs@users.noreply.github.com>
2024-07-30 20:57:25 +0300

    HealthcareFinal.py

--- HealthcareFinal.py
+++ HealthcareFinal.py
@@ -3,3 +3,4 @@ import pandas as pd
 import plotly.express as px
 st.title("Assessing the Risk Factors on Type 2 Diabetes in the North Africa and Middle East Region")
 df = pd.read_csv("https://github.com/hadilfs/Healthcare/blob/main/IHME-GBD_2021_DATA-7e856563-1.csv", on_bad_lines='skip')  # Handles problematic lines
+st.write(df.head())
