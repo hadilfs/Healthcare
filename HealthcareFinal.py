9c8b52934c8bc48401a3839536a46a47701cd05e
hadilfs <145138634+hadilfs@users.noreply.github.com>
2024-07-30 20:46:09 +0300

    HealthcareFinal.py

--- HealthcareFinal.py
+++ HealthcareFinal.py
@@ -6,4 +6,4 @@ try:
     df = pd.read_csv("https://github.com/hadilfs/Healthcare/blob/main/IHME-GBD_2021_DATA-7e856563-1.csv", on_bad_lines='skip')  # Handles problematic lines
     print(df.head())
 except pd.errors.ParserError as e:
-    print(f"ParserError: {e}")
+    print("OOPS")
