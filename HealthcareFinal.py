2b69a2857353c13772c218ec13d7140b4b9fcbe6
hadilfs <145138634+hadilfs@users.noreply.github.com>
2024-07-30 19:24:01 +0300

    HealthcareFinal.py

--- HealthcareFinal.py
+++ HealthcareFinal.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-df = pd.read_csv("https://raw.githubusercontent.com/hadilfs/mortality2/main/lebanon_mortality_2.csv")
+df = pd.read_csv("IHME-GBD_2021_DATA-7e856563-1.csv")
