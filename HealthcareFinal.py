89cb48860b459f228fda2229262a4178750cfdb5
hadilfs <145138634+hadilfs@users.noreply.github.com>
2024-07-30 20:22:22 +0300

    HealthcareFinal.py

--- HealthcareFinal.py
+++ HealthcareFinal.py
@@ -4,6 +4,7 @@ import plotly.express as px
 st.title("Assessing the Risk Factors on Type 2 Diabetes in the North Africa and Middle East Region")
 try:
     df = pd.read_csv("https://github.com/hadilfs/Healthcare/blob/main/IHME-GBD_2021_DATA-7e856563-1.csv", on_bad_lines='skip')  # Handles problematic lines
-    st.print(df.head())
+    print(df.head())
 except pd.errors.ParserError as e:
     print(f"ParserError: {e}")
+st.title(df)
