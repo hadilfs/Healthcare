7cd8efdb2792926f38ec83393d0ef17e02be4bdb
hadilfs <145138634+hadilfs@users.noreply.github.com>
2024-07-30 19:25:22 +0300

    HealthcareFinal.py

--- HealthcareFinal.py
+++ HealthcareFinal.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-df = pd.read_csv("IHME-GBD_2021_DATA-7e856563-1.csv")
+df = pd.read_csv("https://github.com/hadilfs/Healthcare/blob/main/IHME-GBD_2021_DATA-7e856563-1.csv")
