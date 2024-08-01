77abbf27c16c0d4bbc4e01a982797444012094d8
hadilfs <145138634+hadilfs@users.noreply.github.com>
2024-08-02 02:44:42 +0300

    Update HealthcareFinal.py

--- HealthcareFinal.py
+++ HealthcareFinal.py
@@ -18,8 +18,7 @@ page = st.sidebar.radio("Go to", ["Home", "EDA", "Conclusion"])
 # Home page
 if page == "Home":
      st.image("https://raw.githubusercontent.com/hadilfs/Healthcare/main/AUBlogo.png", use_column_width=True)
- # Center the title
-    st.markdown("<h1 style='text-align: center;'>Diabetes Type 2 Mortality in the MENA Region: A Comparative Analysis of Regional and Global Trends</h1>", unsafe_allow_html=True)
+     st.markdown("<h1 style='text-align: center;'>Diabetes Type 2 Mortality in the MENA Region: A Comparative Analysis of Regional and Global Trends</h1>", unsafe_allow_html=True)
     
     # Center the subtitle and names
     st.markdown("""
