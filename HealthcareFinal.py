960c7b9ab565d839f2d62627ecc2b2d28b09f836
hadilfs <145138634+hadilfs@users.noreply.github.com>
2024-08-02 03:34:05 +0300

    Update HealthcareFinal.py

--- HealthcareFinal.py
+++ HealthcareFinal.py
@@ -5,6 +5,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load your data
+import pandas as pd
+import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+
 df = pd.read_csv("https://raw.githubusercontent.com/hadilfs/Healthcare/main/combined_data.csv")
 df = df[df['val'] > 0]
 
@@ -17,9 +23,9 @@ page = st.sidebar.radio("Go to", ["Home", "EDA", "Conclusion"])
 
 # Home page
 if page == "Home":
-     st.image("https://raw.githubusercontent.com/hadilfs/Healthcare/main/AUBlogo.png", use_column_width=True)
-     st.markdown("<h1 style='text-align: center;'>Diabetes Type 2 Mortality in the MENA Region: A Comparative Analysis of Regional and Global Trends</h1>", unsafe_allow_html=True)
-     st.markdown("""
+    st.image("https://raw.githubusercontent.com/hadilfs/Healthcare/main/AUBlogo.png", use_column_width=True)
+    st.markdown("<h1 style='text-align: center;'>Diabetes Type 2 Mortality in the MENA Region: A Comparative Analysis of Regional and Global Trends</h1>", unsafe_allow_html=True)
+    st.markdown("""
     <div style='text-align: center;'>
         <h2>Healthcare Analytics</h2>
         <h3>Dr. Samar El-hajj</h3>
@@ -28,7 +34,7 @@ if page == "Home":
         <strong>Rawan Hallal</strong></p>
     </div>
     """, unsafe_allow_html=True)
-    
+
 # EDA page
 elif page == "EDA":
     st.title("Exploratory Data Analysis")
@@ -41,7 +47,7 @@ elif page == "EDA":
     plt.ylabel('Deaths (Percent)')
     plt.yticks([])
     st.pyplot(plt)
-    st.write("Explanation")
+    st.write("**Explanation**")
 
     st.subheader("Distribution of Deaths by Age Group")
     df_sorted = df1.sort_values('age')
@@ -52,7 +58,7 @@ elif page == "EDA":
     plt.ylabel('Deaths (Percent)')
     plt.yticks([])
     st.pyplot(plt)
-    st.write("Explanation")
+    st.write("**Explanation**")
     
     st.subheader("Deaths by Diabetes Type 2 Over Time Globally")
     mena_data = df1.groupby('year')['val'].sum().reset_index()
@@ -64,7 +70,7 @@ elif page == "EDA":
     plt.yticks([])
     plt.grid(False)
     st.pyplot(plt)
-     st.write("Explanation")
+    st.write("**Explanation**")
     
     st.subheader("Distribution of Risk Factors Globally")
     risk_factors = df1.groupby('rei')['val'].sum().reset_index()
@@ -83,7 +89,8 @@ elif page == "EDA":
         margin=dict(t=50, b=50, l=50, r=50)
     )
     st.plotly_chart(fig)
-    st.write("")
+    st.write("**Explanation**")
+    
     st.subheader("Correlation Between Risk Factors and Deaths by Diabetes Type 2")
     plt.figure(figsize=(12, 6))
     correlation_matrix = df1.pivot_table(index='location', columns='rei', values='val').corr()
@@ -103,8 +110,8 @@ elif page == "EDA":
     plt.xticks([])
     plt.grid(axis='y', linestyle='', alpha=0.7)
     st.pyplot(plt)
-    st.write("Explanation")
-
+    st.write("**Explanation**")
+    
     st.subheader("Total Deaths by Year in the MENA Region")
     mena_data = df.groupby('year')['val'].sum().reset_index()
     plt.figure(figsize=(10, 6))
@@ -154,54 +161,24 @@ elif page == "EDA":
 
 # Conclusion page
 elif page == "Conclusion":
-     st.title("Conclusion and Findings")
-     st.image("https://raw.githubusercontent.com/hadilfs/Healthcare/main/diabetesImage.jpg", use_column_width=True)
-     st.subheader("1. Gender Differences in Diabetes Type 2 Mortality:")
-    st.write(
-        "The side-by-side boxplot analysis reveals that mortality rates due to diabetes Type 2 are similar across genders, "
-        "indicating that both males and females are equally affected. This suggests that public health interventions should "
-        "adopt a gender-neutral approach, focusing on comprehensive strategies that address the broader population rather than targeting specific genders."
-    )
+    st.title("Conclusion and Findings")
+    st.image("https://raw.githubusercontent.com/hadilfs/Healthcare/main/diabetesImage.jpg", use_column_width=True)
     
-    st.subheader("2. Age Group Analysis:")
-    st.write(
-        "The data highlights a troubling trend with the highest percentage of deaths occurring in the 20-24 years age group. "
-        "This alarming finding underscores the need for targeted awareness programs and health initiatives aimed at young adults. "
-        "Enhancing early detection, promoting healthy lifestyles, and implementing educational programs in schools and colleges are crucial steps in addressing this issue."
-    )
+    st.write("""
+    **1. Gender Differences in Diabetes Type 2 Mortality:**
+    The side-by-side boxplot analysis reveals that mortality rates due to diabetes Type 2 are similar across genders, indicating that both males and females are equally affected. This suggests that public health interventions should adopt a gender-neutral approach, focusing on comprehensive strategies that address the broader population rather than targeting specific genders.
     
-    st.subheader("3. Global Trends in Diabetes Type 2 Mortality:")
-    st.write(
-        "The global analysis shows a rising trend in diabetes Type 2 mortality from 2011 to 2020, with a notable dip in 2018 followed by a sharp increase. "
-        "This suggests a growing global burden of diabetes, with potential impacts from external factors such as pandemics. "
-        "Strengthening global health initiatives, improving data quality, and advocating for effective public health policies are essential to address this trend."
-    )
+    **2. Age Group Analysis:**
+    The data highlights a troubling trend with the highest percentage of deaths occurring in the 20-24 years age group. This alarming finding underscores the need for targeted awareness programs and health initiatives aimed at young adults. Enhancing early detection, promoting healthy lifestyles, and implementing educational programs in schools and colleges are crucial steps in addressing this issue.
     
-    st.subheader("4. Regional Risk Factors in MENA:")
-    st.write(
-        "The treemap and heatmap analyses highlight that high body-mass index (BMI) is the dominant risk factor in the MENA region, "
-        "with significant correlations to poor dietary choices and low physical activity. Addressing obesity through lifestyle interventions, "
-        "promoting healthier diets, and increasing physical activity are vital for reducing diabetes-related deaths in the region."
-    )
+    **3. Global Trends in Diabetes Type 2 Mortality:**
+    The global analysis shows a rising trend in diabetes Type 2 mortality from 2011 to 2020, with a notable dip in 2018 followed by a sharp increase. This suggests a growing global burden of diabetes, with potential impacts from external factors such as pandemics. Strengthening global health initiatives, improving data quality, and advocating for effective public health policies are essential to address this trend.
     
-    st.subheader("5. Regional and Country-Specific Trends:")
-    st.write(
-        "The horizontal bar chart and regional trend analysis reveal varying mortality rates across regions and countries. "
-        "North America and MENA exhibit high mortality rates, while East Asia and Sub-Saharan Africa show lower rates. "
-        "The need for tailored public health strategies and regional collaborations is evident to effectively manage diabetes across different socio-economic and cultural contexts."
-    )
+    **4. Regional Risk Factors in MENA:**
+    The treemap and heatmap analyses highlight that high body-mass index (BMI) is the dominant risk factor in the MENA region, with significant correlations to poor dietary choices and low physical activity. Addressing obesity through lifestyle interventions, promoting healthier diets, and increasing physical activity are vital for reducing diabetes-related deaths in the region.
     
-    st.subheader("6. Healthcare System Challenges and Strategies:")
-    st.write(
-        "Access to care, quality of healthcare, and financial burdens are key challenges in the MENA region. Implementing telemedicine, "
-        "improving healthcare quality through standardized protocols and training, and reducing costs through generic medications and preventive care are crucial strategies. "
-        "Expanding health insurance, fostering public-private partnerships, and leveraging digital health solutions will further support effective diabetes management."
-    )
+    **5. Regional and Country-Specific Trends:**
+    The horizontal bar chart and regional trend analysis reveal varying mortality rates across regions and countries. North America and MENA exhibit high mortality rates, while East Asia and Sub-Saharan Africa show lower rates. The need for tailored public health strategies and regional collaborations is evident to effectively manage diabetes across different socio-economic and cultural contexts.
     
-    st.write(
-        "### Conclusion Summary:\n"
-        "The findings from the Exploratory Data Analysis (EDA) underscore the complexity of diabetes Type 2 mortality across different demographics and regions. "
-        "Addressing this global health challenge requires a multi-faceted approach that includes gender-neutral interventions, targeted age-specific programs, "
-        "global and regional health initiatives, and comprehensive healthcare strategies. By focusing on both clinical and economic aspects, we can better manage and reduce "
-        "the impact of diabetes Type 2, ultimately improving health outcomes and reducing the burden on healthcare systems worldwide."
-    )
+    **6. Healthcare System Challenges and Strategies:**
+    Access to care, quality of healthcare,
