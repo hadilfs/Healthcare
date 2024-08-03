3b02bb834a733c1688ae660559e175c9cf12fd36
hadilfs <145138634+hadilfs@users.noreply.github.com>
2024-08-03 17:26:06 +0300

    Update HealthcareFinal.py

--- HealthcareFinal.py
+++ HealthcareFinal.py
@@ -200,8 +200,8 @@ elif page == "Dashboard":
         )
         fig.update_traces(marker=dict(line=dict(color='rgba(0,0,0,0)', width=0)))
         fig.update_layout(
-            width=1600,
-            height=250,
+            width=1700,
+            height=200,
             margin=dict(t=20, b=20, l=20, r=20)
         )
         st.plotly_chart(fig)
